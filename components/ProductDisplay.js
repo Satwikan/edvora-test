e43a90e1dd4083328fafaeffe0f9bf15da9500c0
satwikan <satwikanmol25@gmail.com>
2022-02-13 14:20:08 +0530

    removed error

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -5,13 +5,13 @@ const ProductDisplay = ({ data, brands }) => {
   return (
     <div className="flex flex-col space-y-4">
       {brands.map((brand, idx) => (
-        <div id={idx}>
+        <div key={idx}>
           <p className="text-2xl pb-4">{brand}</p>
           <hr className="text-2xl py-4"/>
           <div className="w-screen-90 lg:w-screen-80 overflow-x-scroll whitespace-nowrap bg-black flex items-center space-x-4 py-6 px-8 rounded-md">
             {data.map((product, idx) => {
               return product.brand_name === brand ? (
-                <ProductCard product={product} id={idx} />
+                <ProductCard product={product} key={idx} />
               ) : (
                 <></>
               );
