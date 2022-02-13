adee84fa729b6c339f338f885b3042b00d4003a8
satwikan <satwikanmol25@gmail.com>
2022-02-13 14:13:41 +0530

    .

--- components/Filters.js
+++ components/Filters.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const Filters = ({brands, city, state}) => {
   return (
-    <div className="bg-black flex flex-col space-y-4 p-6 rounded-lg h-full w-[228px] pt-12 pb-8">
+    <div className="bg-black flex flex-col space-y-4 p-6 rounded-lg h-full w-[228px] pb-8">
       <p className="text-[#a5a5a5] border-white border-b-2 h-6 pb-8 mb-4">Filter</p>
       <DropDown name="Products" list={brands} />
       <DropDown name="State" list={state} />
